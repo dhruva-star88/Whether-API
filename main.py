32054ed397e128abfe18385d1c76fb3ccd2c2808
dhruva-star88 <dhruvatej6565@gmail.com>
2024-02-23 16:33:14 +0530

    Added data and added dynamic functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -16,7 +17,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temp = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temp = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {
         "station": station,
         "date": date,
